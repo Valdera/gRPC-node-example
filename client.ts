c4119def2a10181f7aba0acd462d95b8dae3943b
Valdera <f.valdera@yahoo.co.id>
2022-01-16 23:02:21 +0700

    complete all example

---
+++ client.ts
@@ -0,0 +1,93 @@
+import path from "path";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import { ProtoGrpcType } from "./proto/random";
+import readline from "readline";
+import { validateLoadBalancingConfig } from "@grpc/grpc-js/build/src/load-balancer";
+
+const PORT = 8082;
+const PROTO_FILE = "./proto/random.proto";
+
+const packageDef = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE));
+const grpcObj = grpc.loadPackageDefinition(
+  packageDef
+) as unknown as ProtoGrpcType;
+
+const client = new grpcObj.randomPackage.Random(
+  `0.0.0.0:${PORT}`,
+  grpc.credentials.createInsecure()
+);
+
+const deadline = new Date();
+deadline.setSeconds(deadline.getSeconds() * 5);
+client.waitForReady(deadline, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  onClientReady();
+});
+
+function onClientReady() {
+  //   client.PingPong({ message: "Ping" }, (err, result) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     console.log(result);
+  //   });
+  //
+  //
+  //   const stream = client.RandomNumbers({ maxVal: 85 });
+  //   stream.on("data", (chunk) => {
+  //     console.log(chunk);
+  //   });
+  //   stream.on("end", () => {
+  //     console.log("communication ended");
+  //   });
+  //
+  //
+  //   const stream = client.TodoList((err, result) => {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     console.log(result);
+  //   });
+  //   stream.write({ todo: "walk the wife", status: "Never" });
+  //   stream.write({ todo: "walk the dog", status: "Doing" });
+  //   stream.write({ todo: "get a real job", status: "Impossible" });
+  //   stream.write({ todo: "feed the dog", status: "Done" });
+  //   stream.end();
+  //
+  //
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const username = process.argv[2];
+  if (!username) console.error("No username, can't join chat"), process.exit();
+
+  const metadata = new grpc.Metadata();
+  metadata.set("username", username);
+  const call = client.Chat(metadata);
+
+  call.write({
+    message: "register"
+  });
+
+  call.on("data", (chunk) => {
+    console.log(`${chunk.username} ==> ${chunk.message}`);
+  });
+
+  rl.on("line", (line) => {
+    if (line === "quit") {
+      call.end();
+    } else {
+      call.write({
+        message: line
+      });
+    }
+  });
+}
---
+++ proto/randomPackage/PingRequest.ts
@@ -0,0 +1,12 @@
+// Original file: proto/random.proto
+
+
+export interface PingRequest {
+  'message'?: (string);
+  'val'?: (number);
+}
+
+export interface PingRequest__Output {
+  'message'?: (string);
+  'val'?: (number);
+}
